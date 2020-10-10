b3a27ec487caa252315706a4b8307dce1e123973
BekahHW <rhawrot@gmail.com>
2020-10-10 05:47:12 -0400

    Add file and ideas for profile screen and del node_modules

---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+// User picture - circle
+// days in a row tracked
+// get support -> button that immediately sends a message to support system
+// Get help now -> national suicide hotline?
