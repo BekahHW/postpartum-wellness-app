a377450df53ad23d7511fec66654098238524b4e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2021-01-30 23:54:40 -0500

    Add plugins to babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+export default function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-  };
+    
+      plugins: [
+        [
+           'module-resolver',
+           {
+             root: ['./src'],
+             extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+             alias: {
+               "tests": ["./tests/"],
+               "@components": "./src/components",
+             }
+           }
+        ]
+      ]
+    }
 };
