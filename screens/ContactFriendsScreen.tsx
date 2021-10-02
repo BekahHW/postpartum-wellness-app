9292350d78609f1bcf13937162aea6caf47f943e
Arvin Fernandez <arvinf07@gmail.com>
2021-10-02 09:59:40 -0400

    created basic functional component

---
+++ screens/ContactFriendsScreen.tsx
@@ -0,0 +1,19 @@
+import { View } from "../components/Themed";
+
+
+export default function ContactFriendsScreen(){
+
+  return(
+    <View>
+
+    <h1>Connection for when you need it most.</h1>
+    
+    <h2>
+      Sometimes life can get hard fast. Staying connected with others can help. 
+      Add the phone number of a friend, partner, or a family member if you need to connect easily. 
+      (We'll never share this information. It is for your use only.)
+    </h2>
+
+    </View>
+  )
+}
