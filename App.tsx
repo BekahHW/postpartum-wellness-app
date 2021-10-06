0f575bc10b71281a80d66d5050f0d1ba0dd142c2
Arvin Fernandez <arvinf07@gmail.com>
2021-10-06 16:29:56 -0400

    Basic functinality for ContactFriend component

--- App.tsx
+++ App.tsx
@@ -6,13 +6,19 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import ContactFriendsScreen from "./screens/ContactFriendsScreen";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  // Just to test ContactFriend component
+  const contactFriend = true
+
   if (!isLoadingComplete) {
     return null;
+  } else if(contactFriend){
+    return(<ContactFriendsScreen />)
   } else {
     return (
       <SafeAreaProvider>
--- screens/ContactFriendsScreen.tsx
+++ screens/ContactFriendsScreen.tsx
@@ -1,19 +1,115 @@
-import { View } from "../components/Themed";
+import { StyleSheet, Text, View, ScrollView, Image} from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+import React, { useState } from "react";
 
 
-export default function ContactFriendsScreen(){
+
+export default function ContactFriendsScreen({}){
+
+  const [contactName, setContactName] = useState("SAMHSA’s National Helpline")
+  const [contactNumber, setContactNumber] = useState("1-800-662-4357")
+
 
   return(
-    <View>
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.getStartedContainer}>
+          <Text style={styles.getStartedText}>Connection for when you need it most.</Text>
 
-    <h1>Connection for when you need it most.</h1>
-    
-    <h2>
-      Sometimes life can get hard fast. Staying connected with others can help. 
-      Add the phone number of a friend, partner, or a family member if you need to connect easily. 
-      (We'll never share this information. It is for your use only.)
-    </h2>
+          <Text style={styles.describe}>
+            Sometimes life can get hard fast. Staying connected with others can help. 
+            Add the phone number of a friend, partner, or a family member if you need to connect easily.
+            (We'll never share this information. It is for your use only.)
+          </Text>
 
+          <Image 
+            source={{
+              uri: "https://image.freepik.com/free-vector/refer-friend-concept-with-hands-holding-phone-with-list-friends-contacts-friend-concept-international-communication-online-chat_214151-34.jpg",
+              width: 200,
+              height: 200
+            }}
+            >
+          </Image>
+      
+          <Text style={styles.describe}>
+            Name of Contact
+          </Text>
+          <TextInput
+            style={styles.textInput}
+            mode="outlined"
+            value={contactName}
+            onChangeText={(text) => setContactName(text)}
+          />
+
+
+          <Text style={styles.describe}>
+            Phone Number of Contact
+          </Text>
+          <TextInput
+            style={styles.textInput}
+            mode="outlined"
+            value={contactNumber}
+            onChangeText={(text) => setContactNumber(text)}
+          />
+
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={() =>
+              console.log("submit", contactName, contactNumber)
+            }
+          >
+            Add Contact!
+          </Button>
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  contentContainer: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: "#fff",
+    paddingBottom: 50,
+  },
+  getStartedContainer: {
+    justifyContent: "center",
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 24,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center",
+    marginBottom: 16,
+  },
+  textInput: {
+    minWidth: 200,
+    height: 64,
+    justifyContent: "center",
+    marginBottom: 8,
+  },
+  button: {
+    minWidth: 200,
+    height: 64,
+    justifyContent: "center",
+    marginBottom: 16,
+    marginTop: 8,
+  },
+  describe: {
+    fontSize: 16,
+    color: "rgba(96,100,109, 1)",
+    marginBottom: 5,
+    marginTop: 5,
+  },
+});
