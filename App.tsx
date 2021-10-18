8b33ecceb667d52bcbf09dde1ff6bb845c477d8b
BekahHW <34313413+BekahHW@users.noreply.github.com>
2021-10-18 13:56:21 -0400

    Remove ContactFriend from app.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
-import ContactFriendsScreen from "./screens/ContactFriendsScreen";
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  // Just to test ContactFriend component
-  const contactFriend = true
-
   if (!isLoadingComplete) {
     return null;
-  } else if(contactFriend){
-    return(<ContactFriendsScreen />)
   } else {
     return (
       <SafeAreaProvider>
