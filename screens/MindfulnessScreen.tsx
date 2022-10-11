31202e2daaea19aba1b2adf65af60efc80083a26
aishwarya-mali <aishwaryamali29@gmail.com>
2022-10-11 13:16:32 +0530

    fix prettier issues

--- screens/MindfulnessScreen.tsx
+++ screens/MindfulnessScreen.tsx
@@ -1,22 +1,24 @@
-import * as React from "react";
-import {
-  StyleSheet,
-  View,
-  Text
-} from "react-native";
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function MindfulnessScreen() {
-  const mindfulnessActivity = "Let's be present at this moment through all of our senses:"
-  const mindfulnessQuestions = ['What do you hear?', 'What do you see?', 'What can you taste?',
-  'What can you smell?', 'What can you feel']
+  const mindfulnessActivity =
+    "Let's be present at this moment through all of our senses:";
+  const mindfulnessQuestions = [
+    'What do you hear?',
+    'What do you see?',
+    'What can you taste?',
+    'What can you smell?',
+    'What can you feel',
+  ];
   return (
     <View style={styles.container}>
       <View style={styles.getStartedContainer}>
-        <Text style={styles.getStartedText}>
-          {mindfulnessActivity}
-        </Text>
-        {mindfulnessQuestions.map(q=>(
-          <Text style={styles.getStartedList}>{'\u2022'} {q}</Text>
+        <Text style={styles.getStartedText}>{mindfulnessActivity}</Text>
+        {mindfulnessQuestions.map((q) => (
+          <Text style={styles.getStartedList}>
+            {'\u2022'} {q}
+          </Text>
         ))}
       </View>
     </View>
@@ -26,22 +28,22 @@ export default function MindfulnessScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
   getStartedContainer: {
     marginHorizontal: 50,
   },
   getStartedText: {
     fontSize: 24,
-    color: "rgba(96,100,109, 1)",
+    color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     marginBottom: 16,
   },
   getStartedList: {
     fontSize: 20,
-    color: "rgba(96,100,109, 1)",
+    color: 'rgba(96,100,109, 1)',
     lineHeight: 20,
     marginBottom: 2,
   },
